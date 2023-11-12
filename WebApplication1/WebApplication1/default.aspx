efaf2f7eee90d35152d20178ae8b41a1762b247d
krishnamoorthy81241 <krishnamoorthy81241@gmail.com>
2023-11-12 14:23:19 +0530

    Add project files.

---
+++ WebApplication1/WebApplication1/default.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WebApplication1._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table style="margin:auto;border:5px solid black">
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="Student ID" Font-Bold="True" Font-Size="Medium"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtStudentID" runat="server" Font-Size="Medium"></asp:TextBox>
+                    &nbsp;
+                        <asp:Button ID="txtGet" runat="server" Text="Get" Font-Bold="True" Font-Size="Medium" OnClick="btnGet_Click" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label ID="Label2" runat="server" Text="Student Name" Font-Bold="True" Font-Size="Medium"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtStudentName" runat="server" Font-Size="Medium"></asp:TextBox>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label ID="Label3" runat="server" Text="Address" Font-Bold="True" Font-Size="Medium"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="ddlAddress" runat="server" Font-Size="Medium" OnSelectedIndexChanged="ddlAddress_SelectedIndexChanged">
+                            <asp:ListItem>INDIA</asp:ListItem>
+                            <asp:ListItem>USA</asp:ListItem>
+                            <asp:ListItem>UK</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label ID="Label4" runat="server" Text="Age" Font-Bold="True" Font-Size="Medium"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtAge" runat="server" Font-Size="Medium"></asp:TextBox>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:Label ID="Label5" runat="server" Text="Contact" Font-Bold="True" Font-Size="Medium"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtContact" runat="server" Font-Size="Medium"></asp:TextBox>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+
+                    </td>
+                    <td>
+                        <asp:Button ID="btnInsert" runat="server" Text="Insert" Font-Bold="True" Font-Size="Medium" OnClick="btnInsert_Click" />
+                    &nbsp;
+                        <asp:Button ID="btnUpdate" runat="server" Text="Update" Font-Bold="True" Font-Size="Medium" OnClick="btnUpdate_Click" />
+                    &nbsp;
+                        <asp:Button ID="btnDelete" runat="server" Text="Delete" Font-Bold="True" Font-Size="Medium" OnClientClick="return confirm('Are You Sure To Delete?');" OnClick="btnDelete_Click" />
+                    &nbsp;
+                        <asp:Button ID="btnSearch" runat="server" Text="Search" Font-Bold="True" Font-Size="Medium" OnClick="btnSearch_Click" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+
+                    </td>
+                    <td>
+                        <asp:GridView ID="GridView1" runat="server" Font-Bold="True" Width="955px"></asp:GridView>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/default.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication1
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadRecord();
+            }
+        }
+
+        SqlConnection sqlConnection = new SqlConnection("Data Source = KRISHNA; Initial Catalog = ProgramDB; Integrated Security = True");
+        protected void BtnInsert_Click(object sender, EventArgs e)
+        {
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("Insert into StudentInfo_Tab values('" + int.Parse(txtStudentID.Text) + "','" + txtStudentName.Text + "','" + ddlAddress.SelectedValue + "','" + float.Parse(txtAge.Text) + "','" + txtContact.Text + "')", sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Inserted');", true);
+            LoadRecord();
+        }
+
+        void LoadRecord()
+        {
+            SqlCommand sqlCommand = new SqlCommand("select * from StudentInfo_Tab", sqlConnection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            GridView1.DataSource = dataTable;
+            GridView1.DataBind();
+        }
+
+        protected void DdlAddress_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("Update StudentInfo_Tab set StudentName = '" + txtStudentName.Text + "', Address = '" + ddlAddress.SelectedValue + "', Age = '" + float.Parse(txtAge.Text) + "', Contact = '" + txtContact.Text + "' where StudentID = '" + int.Parse(txtStudentID.Text) + "'", sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Updated');", true);
+            LoadRecord();
+        }
+
+        protected void BtnDelete_Click(object sender, EventArgs e)
+        {
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("Delete StudentInfo_Tab where StudentID = '" + int.Parse(txtStudentID.Text) + "'", sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Deleted');", true);
+            LoadRecord();
+        }
+
+        protected void BtnSearch_Click(object sender, EventArgs e)
+        {
+            SqlCommand sqlCommand = new SqlCommand("select * from StudentInfo_Tab where studentID = '" + int.Parse(txtStudentID.Text) + "'", sqlConnection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            GridView1.DataSource = dataTable;
+            GridView1.DataBind();
+        }
+
+        protected void BtnGet_Click(object sender, EventArgs e)
+        {
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("select * from StudentInfo_Tab where studentID = '" + int.Parse(txtStudentID.Text) + "'", sqlConnection);
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            while (sqlDataReader.Read())
+            {
+                txtStudentName.Text = sqlDataReader.GetValue(1).ToString();
+                ddlAddress.SelectedValue = sqlDataReader.GetValue(2).ToString();
+                txtAge.Text = sqlDataReader.GetValue(3).ToString();
+                txtContact.Text = sqlDataReader.GetValue(4).ToString();
+            }
+        }
+    }
+}
